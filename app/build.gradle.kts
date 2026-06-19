2d290f71d7097a747e2771ab58aa252885f7b6ba
Tobibur Rahman <tobiburofficial@gmail.com>
2026-06-19 11:19:05 +0530

    Bump version to 1.1.1 (versionCode 3) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.tobibur.journey"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
